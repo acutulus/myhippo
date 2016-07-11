788682049749bb4c0a8b513e1be8d93f66ceb637
Edward Parsons <edparsons2@gmail.com>
2016-07-11 10:17:04 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+angular.module("myHippoPublicData", [])
+.controller("PublicDataController", function($scope) {
+
+    $scope.publicDataData = {form:{},submitted:false};
+
+    $scope.printModel = function() {
+        console.log($scope.publicDataData);
+        $scope.publicDataData.submitted = true;
+    };
+
+    $scope.isValid = function() {
+        var count = 0;
+        for (var i in $scope.publicDataData.form) {
+            if ($scope.publicDataData.form.hasOwnProperty(i)) {
+                count++;
+            }
+        }
+        return $scope.publicDataForm.$valid && count === 12;
+    };
+
+});
---
+++ js/getmdl-select.js
@@ -0,0 +1,74 @@
+{
+    'use strict';
+    window.onload = function () {
+        getmdlSelect.init('.getmdl-select');
+        document.addEventListener("DOMNodeInserted", function (ev) {
+            componentHandler.upgradeDom();
+        }, false);
+    };
+
+    var getmdlSelect = {
+        addEventListeners: function (dropdown) {
+            var input = dropdown.querySelector('input');
+            var list = dropdown.querySelectorAll('li');
+            var clickOpenEvt = null;
+
+            var show = function(evt) {
+              dropdown.classList.add("getmdl-select-visible");
+
+              var callback = function (e) {
+                  // Check to see if the document is processing the same event that
+                  // displayed the menu in the first place. If so, do nothing.
+                  // Also check to see if the menu is in the process of closing itself, and
+                  // do nothing in that case.
+                  // Also check if the clicked element is a menu item
+                  // if so, do nothing.
+                  if (e !== evt && e !== clickOpenEvt && !this.closing_ && e.target.parentNode !== this.element_) {
+                    dropdown.classList.remove("is-focused");
+                    document.removeEventListener('click', callback);
+                    setTimeout(function() {
+                      dropdown.classList.remove("getmdl-select-visible");
+                    }, 250);
+                  }
+              }.bind(this);
+              document.addEventListener('click', callback);
+            };
+
+            input.onclick = function(evt) {
+              clickOpenEvt = evt;
+              show(evt);
+            };
+
+            input.onfocus = function(evt) {
+              show(evt);
+              var menu = dropdown.querySelector('.mdl-menu');
+              menu.MaterialMenu.show();
+            };
+
+            [].forEach.call(list, function (li) {
+                li.onclick = function () {
+
+                    dropdown.MaterialTextfield.change(li.textContent); // handles css class changes
+                    
+                    // update input with the "id" value
+                    input.dataset.val = li.dataset.val || '';
+
+                    if ("createEvent" in document) {
+                        var evt = document.createEvent("HTMLEvents");
+                        evt.initEvent("change", false, true);
+                        input.dispatchEvent(evt);
+                    } else {
+                        input.fireEvent("onchange");
+                    }
+                };
+            });
+        },
+        init: function (selector) {
+            var dropdowns = document.querySelectorAll(selector);
+            [].forEach.call(dropdowns, function (i) {
+                getmdlSelect.addEventListeners(i);
+                i.querySelector('.getmdl-select-container').style.width = i.querySelector('.mdl-menu').clientWidth + 'px';
+            });
+        }
+    };
+}
